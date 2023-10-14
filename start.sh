4c7c0af21630c7571df279cf56219f186ca80f13
lvjing2 <leojames.googol@gmail.com>
2023-10-14 22:17:45 +0800

    modify for deploy

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
-
+#!/bin/sh
 cd ./service
-nohup pnpm start > service.log &
+nohup pnpm start > ../logs/service.log &
 echo "Start service complete!"
 
 
 cd ..
 echo "" > front.log
-nohup pnpm dev > front.log &
+nohup pnpm dev > ./logs/front.log &
 echo "Start front complete!"
-tail -f front.log
